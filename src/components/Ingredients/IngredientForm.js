f336db2005df1cafcc0c2e0041f0373b84ee0df7
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-05-14 19:28:43 -0400

    added a functionality to pass State data throught components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,12 +4,12 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [ enteredTitle, setEnteredTitle ] = useState('');
-  const [ enteredAmount, setEnteredAmount ] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -25,7 +25,7 @@ const IngredientForm = React.memo(props => {
               onChange={event => {
                 setEnteredTitle(event.target.value);
               }}
-          />
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
@@ -35,7 +35,8 @@ const IngredientForm = React.memo(props => {
               value={enteredAmount}
               onChange={event => {
                 setEnteredAmount(event.target.value);
-              }}/>
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
