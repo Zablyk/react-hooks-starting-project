8eb8ec82adc54a1ede1346c54478a815badb15f0
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-05-19 17:57:17 -0400

    added functionality for a Filter By Title field

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -27,6 +27,10 @@ const Ingredients = () => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
   
+  const filteredIngredientsHandler = filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  } 
+
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-starting-pro-e28a1.firebaseio.com/ingredients.json', {
       method: 'POST',
@@ -51,7 +55,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         <IngredientList ingredients={userIngredients} onRemoveItem={(id) => {removeIngredientHandler(id)}}/>
       </section>
     </div>
